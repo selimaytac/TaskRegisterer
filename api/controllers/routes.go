e30b898ba2dc4a7dac1394c1d834979b1319af82
selimaytac <53125635+selimaytac@users.noreply.github.com>
2022-08-30 17:01:29 +0300

    user_controller added.

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -1,5 +1,19 @@
 package controllers
 
-func (server *Server) InitializeRoutes() {
+import "github.com/selimaytac/TaskRegisterer/api/middlewares"
 
+func (s *Server) InitializeRoutes() {
+
+	// Home Route
+	s.Router.HandleFunc("/", middlewares.SetMiddleWareJSON(s.Home)).Methods("GET")
+
+	// Login Route
+	s.Router.HandleFunc("/login", middlewares.SetMiddleWareJSON(s.Login)).Methods("POST")
+
+	// Users routes
+	s.Router.HandleFunc("/users", middlewares.SetMiddleWareJSON(s.CreateUser)).Methods("POST")
+	s.Router.HandleFunc("/users/{page}", middlewares.SetMiddleWareJSON(s.GetUsers)).Methods("GET")
+	s.Router.HandleFunc("/users/{id}", middlewares.SetMiddleWareJSON(s.GetUser)).Methods("GET")
+	s.Router.HandleFunc("/users/{id}", middlewares.SetMiddleWareJSON(middlewares.SetMiddleWareAuthentication(s.UpdateUser))).Methods("PUT")
+	s.Router.HandleFunc("/users/{id}", middlewares.SetMiddleWareAuthentication(s.DeleteUser)).Methods("DELETE")
 }
---
+++ api/controllers/users_controller.go
@@ -0,0 +1,181 @@
+package controllers
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/selimaytac/TaskRegisterer/api/auth"
+	"github.com/selimaytac/TaskRegisterer/api/models"
+	"github.com/selimaytac/TaskRegisterer/api/responses"
+	"github.com/selimaytac/TaskRegisterer/api/utils/formaterror"
+	"io"
+	"net/http"
+	"strconv"
+)
+
+func (server *Server) CreateUser(w http.ResponseWriter, r *http.Request) {
+
+	body, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	user := models.User{}
+	err = json.Unmarshal(body, &user)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	user.Prepare()
+
+	err = user.Validate("")
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	userCreated, err := user.SaveUser(server.DB)
+
+	if err != nil {
+		formattedError := formaterror.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, formattedError)
+		return
+	}
+
+	w.Header().Set("Location", fmt.Sprintf("%s%s/%d", r.Host, r.RequestURI, userCreated.ID))
+	responses.JSON(w, http.StatusCreated, userCreated)
+}
+
+func (server *Server) GetUsers(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	pageNumber, err := strconv.ParseUint(vars["page"], 10, 32)
+
+	user := models.User{}
+
+	users, err := user.FindAllUsers(server.DB, uint32(pageNumber))
+
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, users)
+}
+
+func (server *Server) GetUser(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	uid, err := strconv.ParseUint(vars["id"], 10, 32)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	user := models.User{}
+	userFound, err := user.FindUserByID(server.DB, uint32(uid))
+
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, userFound)
+}
+
+func (server *Server) UpdateUser(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	uid, err := strconv.ParseUint(vars["id"], 10, 32)
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	user := models.User{}
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	tokenID, err := auth.ExtractTokenId(r)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+
+	if tokenID != uint32(uid) {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New(http.StatusText(http.StatusUnauthorized)))
+		return
+	}
+
+	user.Prepare()
+
+	err = user.Validate("update")
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	updatedUser, err := user.UpdateAUser(server.DB, uint32(uid))
+	if err != nil {
+		formattedError := formaterror.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, formattedError)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, updatedUser)
+}
+
+func (server *Server) DeleteUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	user := models.User{}
+
+	uid, err := strconv.ParseUint(vars["id"], 10, 32)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	tokenID, err := auth.ExtractTokenId(r)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+
+	if tokenID != 0 && tokenID != uint32(uid) {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New(http.StatusText(http.StatusUnauthorized)))
+		return
+	}
+
+	_, err = user.DeleteAUser(server.DB, uint32(uid))
+
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	w.Header().Set("Entity", fmt.Sprintf("%d", uid))
+	responses.JSON(w, http.StatusNoContent, "")
+}
